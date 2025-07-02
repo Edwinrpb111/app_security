314523466818140855fcdc2c1020c4d425fa0944
Edwinrpb <pastoredwin_777@hotmail.com>
2025-07-01 21:41:28 -0500

    checkpoint

--- proy_clinico/settings.py
+++ proy_clinico/settings.py
@@ -101,9 +101,9 @@ WSGI_APPLICATION = 'proy_clinico.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'medicos',
+        'NAME': 'BDoctor',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': '1234',
         'HOST': 'localhost',  # o la IP del servidor de base de datos
         'PORT': '5432',        # puerto por defecto de PostgreSQL
     }
